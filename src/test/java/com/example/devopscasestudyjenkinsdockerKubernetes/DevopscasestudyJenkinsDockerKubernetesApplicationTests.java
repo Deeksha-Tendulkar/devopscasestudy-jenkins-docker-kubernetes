8bc4eaa4754c2201070cfff09c3e413743dd1582
Deeksha-Tendulkar <deekshatend97@gmail.com>
2020-05-21 19:08:26 +0530

    committed

---
+++ src/test/java/com/example/devopscasestudyjenkinsdockerKubernetes/DevopscasestudyJenkinsDockerKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopscasestudyjenkinsdockerKubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopscasestudyJenkinsDockerKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
